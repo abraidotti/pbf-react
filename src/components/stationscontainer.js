0f7a78c0fdae630edaeb5d8f1f2e0a5119cc292b
Alessandro Braidotti <alessandro.braidotti@gmail.com>
2018-05-01 15:28:48 -0400

    add barely working stations-getter

--- src/components/stationscontainer.js
+++ src/components/stationscontainer.js
@@ -1,29 +1,30 @@
 import React, {Component} from 'react';
+import { getClosestStations } from '../utils/stations';
 
 export default class StationsContainer extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      closestStations: {}
+    }
   };
 
+  componentDidMount(){
+    let closestStations = getClosestStations(this.props.location, this.props.stations);
+
+    Promise.all([closestStations])
+    .then( (values) => {
+      this.setState({
+        closestStations: values[0],
+      });
+      console.log(this.state.closestStations);
+    })
+  }
+
   render(){
-    console.log(this.props.stations)
-    return(
-      <p>stations go here</p>
+    return this.state.closestStations.length ?
+      <span>stations here</span> : (
+      <span>Loading wells...</span>
     )
-  //   if (!this.props.validLocation){
-  //     return(
-  //       <div id="stations-list">
-  //         <h1>Waiting on location to render closest stations</h1>
-  //       </div>
-  //     )
-  //   };
-  //
-  //   return(
-  //     <div id="stations-list">
-  //       <p>Closest Stations</p>
-  //
-  //       <p>props lat lng = {this.props.latLng}.</p>
-  //     </div>
-  //   )
-  };
+  }
 };
