d297c615476bae7e9f75576c48596b6c4860eb5b
Alessandro Braidotti <alessandro.braidotti@gmail.com>
2018-12-26 10:52:04 -0500

    add weather advisory

--- src/components/weathercontainer.js
+++ src/components/weathercontainer.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 
 export default class WeatherContainer extends Component {
   render() {
-    // smooth out weather conditions and temperature
     let conditions =
       this.props.forecast.weather[0].description.charAt(0).toUpperCase() +
       this.props.forecast.weather[0].description.slice(1);
@@ -11,10 +10,15 @@ export default class WeatherContainer extends Component {
       this.props.forecast.weather[0].icon
     }.png`;
 
+    let advisory = (this.props.forecast.rain || this.props.forecast.snow) ?
+                    <p style={{ color: "#A01E26" }}>Not ideal biking weather.</p> :
+                    <></>
+
     return (
       <div className="text-center">
         <span>{conditions} and {temp}° today.</span>
         <img src={imgString} alt="weather icon" />
+        {advisory}
       </div>
     );
   }
